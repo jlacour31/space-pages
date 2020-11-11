2c80836b23c0ba878cf451559112374bef63de8d
jlacour31 <julienlacour31@gmail.com>
2020-11-11 10:20:59 +0000

    Rebuild pages at 1de3419

--- webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {12:13,8:-1,6:-1,1:2,2:-1,0:-1,13:8,5:-1,7:2,4:6,10:11,9:6,3:-1,11:8};
+return {1:2,6:-1,8:-1,9:6,3:-1,10:11,11:8,12:13,0:-1,4:6,5:-1,2:-1,13:8,7:2};
 });
